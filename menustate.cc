f0829d4346ff4c99e3fbae42cc2c69b69362a39e
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-02-05 18:08:37 -0500

    create menu buttons in menu state

--- menustate.cc
+++ menustate.cc
@@ -1,4 +1,9 @@
 #include "menustate.h"
+#include "game.h"
+#include "loaderparams.h"
+#include "menubutton.h"
+#include "playstate.h"
+#include "texturemanager.h"
 #include <iostream>
 using namespace std;
 
@@ -6,16 +11,65 @@ const string MenuState::menuID = "MENU";
 
 string MenuState::getStateID() const { return menuID; }
 
-void MenuState::update() {}
+void MenuState::update() {
+  for (GameObject *gameObject : gameObjects) {
+    gameObject->update();
+  }
+}
 
-void MenuState::render() {}
+void MenuState::render() {
+  for (GameObject *gameObject : gameObjects) {
+    gameObject->draw();
+  }
+}
 
 bool MenuState::onEnter() {
+  // load textures safely
+  if (!TextureManager::Instance()->load("assets/button.png", "playbutton",
+                                        Game::Instance()->getRenderer())) {
+    return false;
+  }
+
+  if (!TextureManager::Instance()->load("assets/exit.png", "exitbutton",
+                                        Game::Instance()->getRenderer())) {
+    return false;
+  }
+
+  // create game objects
+  // TODO: Use smart pointers
+  GameObject *playButton = new MenuButton(
+      new LoaderParams(100, 100, 400, 100, "playbutton"), menuToPlay);
+  GameObject *exitButton = new MenuButton(
+      new LoaderParams(100, 300, 400, 100, "exitbutton"), exitFromMenu);
+
+  // add game objects to vector
+  gameObjects.push_back(playButton);
+  gameObjects.push_back(exitButton);
+
   cout << "Entering MenuState" << endl;
   return true;
 }
 
 bool MenuState::onExit() {
+  for (GameObject *gameObject : gameObjects) {
+    gameObject->clean();
+  }
+
+  gameObjects.clear();
+
+  TextureManager::Instance()->clearFromTextureMap("playbutton");
+  TextureManager::Instance()->clearFromTextureMap("exitbutton");
+
   cout << "Exiting MenuState" << endl;
   return true;
+}
+
+void MenuState::menuToPlay() {
+  cout << "Play button clicked" << endl;
+  Game::Instance()->getStateMachine()->changeState(new PlayState());
+}
+
+void MenuState::exitFromMenu() {
+  cout << "Exit button clicked" << endl;
+  Game::Instance()->quit();
 }
--- menustate.h
+++ menustate.h
@@ -1,10 +1,22 @@
 #ifndef _MENU_STATE_H_
 #define _MENU_STATE_H_
 
+#include "gameobject.h"
 #include "gamestate.h"
 #include <string>
+#include <vector>
 
 class MenuState : public GameState {
+  static const std::string menuID;
+
+  std::vector<GameObject *>
+      gameObjects; // pointer because we want to use polymorphism
+
+  // callback functions for MenuButtons
+  static void menuToPlay(); // need to be static because our callback
+                            // functionality only supports static functions
+  static void exitFromMenu();
+
 public:
   virtual void update();
   virtual void render();
@@ -14,9 +26,6 @@ public:
 
   // getters
   virtual std::string getStateID() const;
-
-private:
-  static const std::string menuID;
 
 }; // class MenuState
 
