e5fce874613670d2eb5ca015d1756f6557bb54e1
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-02-05 18:10:45 -0500

    format some old files

--- texturemanager.cc
+++ texturemanager.cc
@@ -1,71 +1,76 @@
 #include <iostream>
 // vvv imported in texturemanager.h
 // #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h> // IMG_Load, IMG_GetError
 #include "texturemanager.h"
+#include <SDL2/SDL_image.h> // IMG_Load, IMG_GetError
 using namespace std;
 
 // static member variable `instance` needs to be defined outside of class
 TextureManager *TextureManager::instance = nullptr;
 
-bool TextureManager::load(string filePath, string id, SDL_Renderer *renderer)
-{
-    // IMG_Load returns a surface, but we will convert it to a texture later
-    //   since textures use hardware acceleration
-    SDL_Surface *tempSurface = IMG_Load(filePath.c_str()); // .c_str() because IMG_Load expects a char *
-    if (tempSurface == nullptr)
-    {
-        cout << "IMG_Load Error: " << IMG_GetError() << endl;
-        return false;
-    }
+bool TextureManager::load(string filePath, string id, SDL_Renderer *renderer) {
+  // IMG_Load returns a surface, but we will convert it to a texture later
+  //   since textures use hardware acceleration
+  SDL_Surface *tempSurface =
+      IMG_Load(filePath.c_str()); // .c_str() because IMG_Load expects a char *
+  if (tempSurface == nullptr) {
+    cout << "IMG_Load Error: " << IMG_GetError() << endl;
+    return false;
+  }
 
-    // Convert surface to texture since textures use hardware acceleration
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, tempSurface);
-    // Destroy the temporary surface
-    SDL_FreeSurface(tempSurface);
+  // Convert surface to texture since textures use hardware acceleration
+  SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, tempSurface);
+  // Destroy the temporary surface
+  SDL_FreeSurface(tempSurface);
 
-    if (texture == nullptr)
-    {
-        cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << endl;
-        return false;
-    }
+  if (texture == nullptr) {
+    cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << endl;
+    return false;
+  }
 
-    textureMap[id] = texture;
+  textureMap[id] = texture;
 
-    return true;
+  return true;
 }
 
-void TextureManager::draw(string id, int x, int y, int width, int height, SDL_Renderer *renderer, SDL_RendererFlip flip)
-{
-    SDL_Rect sourceRectangle;
-    SDL_Rect destinationRectangle;
+void TextureManager::draw(string id, int x, int y, int width, int height,
+                          SDL_Renderer *renderer, SDL_RendererFlip flip) {
+  SDL_Rect sourceRectangle;
+  SDL_Rect destinationRectangle;
 
-    sourceRectangle.x = 0;
-    sourceRectangle.y = 0;
-    sourceRectangle.w = destinationRectangle.w = width;
-    sourceRectangle.h = destinationRectangle.h = height;
+  sourceRectangle.x = 0;
+  sourceRectangle.y = 0;
+  sourceRectangle.w = destinationRectangle.w = width;
+  sourceRectangle.h = destinationRectangle.h = height;
 
-    destinationRectangle.x = x;
-    destinationRectangle.y = y;
+  destinationRectangle.x = x;
+  destinationRectangle.y = y;
 
-    SDL_RenderCopyEx(renderer, textureMap[id], &sourceRectangle, &destinationRectangle, 0, 0, flip); // the 0, 0 is for rotation
-                                                                                                     // (we _don't_ want to rotate so we ask the function to
-                                                                                                     // rotate 0 degrees about the centre 0)
+  SDL_RenderCopyEx(renderer, textureMap[id], &sourceRectangle,
+                   &destinationRectangle, 0, 0,
+                   flip); // the 0, 0 is for rotation
+                          // (we _don't_ want to rotate so we ask the function
+                          // to rotate 0 degrees about the centre 0)
 }
 
-void TextureManager::drawFrame(std::string id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer *renderer, SDL_RendererFlip flip)
-{
-    SDL_Rect sourceRectangle;
-    SDL_Rect destinationRectangle;
+void TextureManager::drawFrame(std::string id, int x, int y, int width,
+                               int height, int currentRow, int currentFrame,
+                               SDL_Renderer *renderer, SDL_RendererFlip flip) {
+  SDL_Rect sourceRectangle;
+  SDL_Rect destinationRectangle;
 
-    sourceRectangle.x = width * currentFrame;
-    sourceRectangle.y = height * (currentRow - 1);
-    sourceRectangle.w = destinationRectangle.w = width;
-    sourceRectangle.h = destinationRectangle.h = height;
-    destinationRectangle.x = x;
-    destinationRectangle.y = y;
+  sourceRectangle.x = width * currentFrame;
+  sourceRectangle.y = height * (currentRow - 1);
+  sourceRectangle.w = destinationRectangle.w = width;
+  sourceRectangle.h = destinationRectangle.h = height;
+  destinationRectangle.x = x;
+  destinationRectangle.y = y;
 
-    SDL_RenderCopyEx(renderer, textureMap[id], &sourceRectangle, &destinationRectangle, 0, 0, flip); // the 0, 0 is for rotation
-                                                                                                     // (we _don't_ want to rotate so we ask the function to
-                                                                                                     // rotate 0 degrees about the centre 0)
+  SDL_RenderCopyEx(renderer, textureMap[id], &sourceRectangle,
+                   &destinationRectangle, 0, 0,
+                   flip); // the 0, 0 is for rotation
+                          // (we _don't_ want to rotate so we ask the function
+                          // to rotate 0 degrees about the centre 0)
 }
+
+void TextureManager::clearFromTextureMap(string id) { textureMap.erase(id); }
--- texturemanager.h
+++ texturemanager.h
@@ -1,39 +1,46 @@
 #ifndef _TEXTURE_MANAGER_H_
 #define _TEXTURE_MANAGER_H_
 
+#include <SDL2/SDL.h> // cannot use forward declaration since SDL_RendererFlip is not a pointer
 #include <map>
 #include <string>
-#include <SDL2/SDL.h> // cannot use forward declaration since SDL_RendererFlip is not a pointer
-// vvv Not using this since it would apply to all files that include this header file.
-// using namespace std;
+// vvv Not using this since it would apply to all files that include this header
+// file. using namespace std;
 
 /*
-    TextureManager is a singleton class that manages the loading and unloading of textures.
+    TextureManager is a singleton class that manages the loading and unloading
+   of textures.
 */
 
-class TextureManager
-{
-    static TextureManager *instance;
-    std::map<std::string, SDL_Texture *> textureMap;
-    TextureManager() {} // private constructor to enable the singleton pattern. to instantiate, use the static instance() method instead.
+class TextureManager {
+  static TextureManager *instance;
+  std::map<std::string, SDL_Texture *> textureMap;
+  TextureManager() {} // private constructor to enable the singleton pattern. to
+                      // instantiate, use the static instance() method instead.
 
 public:
-    static TextureManager *Instance()
-    {
-        // construct instance if it doesn't exist, else just return instance (avoids creating multiple TextureManager objects)
-        if (instance == nullptr)
-        {
-            instance = new TextureManager();
-        }
-        return instance;
+  static TextureManager *Instance() {
+    // construct instance if it doesn't exist, else just return instance (avoids
+    // creating multiple TextureManager objects)
+    if (instance == nullptr) {
+      instance = new TextureManager();
     }
-    // loads image at filePath into internal texture directory with the key of `id`
-    bool load(std::string filePath, std::string id, SDL_Renderer *renderer);
-    // renders texture with `id` to renderer at position (x, y) with `width` and `height`
-    // flips it along the axis specified by `flip`
-    void draw(std::string id, int x, int y, int width, int height, SDL_Renderer *renderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
-    // same as draw except renders the frame of a image (spritesheet) at `currentFrame` in row `currentRow`
-    void drawFrame(std::string id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer *renderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+    return instance;
+  }
+  // loads image at filePath into internal texture directory with the key of
+  // `id`
+  bool load(std::string filePath, std::string id, SDL_Renderer *renderer);
+  // renders texture with `id` to renderer at position (x, y) with `width` and
+  // `height` flips it along the axis specified by `flip`
+  void draw(std::string id, int x, int y, int width, int height,
+            SDL_Renderer *renderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+  // same as draw except renders the frame of a image (spritesheet) at
+  // `currentFrame` in row `currentRow`
+  void drawFrame(std::string id, int x, int y, int width, int height,
+                 int currentRow, int currentFrame, SDL_Renderer *renderer,
+                 SDL_RendererFlip flip = SDL_FLIP_NONE);
+  // frees the texture with `id`
+  void clearFromTextureMap(std::string id);
 };
 
 typedef TextureManager TheTextureManager;
