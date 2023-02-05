b77af141f17c4b752cd1bd9b6ee421ee20af20d0
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-02-05 17:18:31 -0500

    mouse input handling

--- inputhandler.cc
+++ inputhandler.cc
@@ -1,7 +1,16 @@
 #include "inputhandler.h"
 #include "game.h"
+#include "vector.h"
 
-InputHandler::InputHandler() {}
+InputHandler::InputHandler() : mousePosition(new Vector2D{2, 2}) {
+  // all mouse buttons are initially released
+  for (int i = 0; i < 3; ++i) {
+    mouseButtonStates.push_back(false);
+  }
+}
+
+// TODO: Does this need to be a pointer?
+InputHandler::~InputHandler() { delete mousePosition; }
 
 InputHandler *InputHandler::instance = nullptr;
 
@@ -23,6 +32,15 @@ void InputHandler::update() {
     case SDL_KEYUP:
       onKeyUp();
       break;
+    case SDL_MOUSEMOTION:
+      onMouseMove(event);
+      break;
+    case SDL_MOUSEBUTTONDOWN:
+      onMouseButtonDown(event);
+      break;
+    case SDL_MOUSEBUTTONUP:
+      onMouseButtonUp(event);
+      break;
     default:
       break;
     }
@@ -31,10 +49,56 @@ void InputHandler::update() {
 
 void InputHandler::clean() {}
 
+void InputHandler::reset() {
+  // reset mouse button states
+  for (int i = 0; i < 3; ++i) {
+    mouseButtonStates[i] = false;
+  }
+}
+
+// handlers
+
 void InputHandler::onKeyUp() { keyStates = SDL_GetKeyboardState(0); }
 
 void InputHandler::onKeyDown() { keyStates = SDL_GetKeyboardState(0); }
 
+void InputHandler::onMouseMove(SDL_Event &event) {
+  mousePosition->setX(event.motion.x);
+  mousePosition->setY(event.motion.y);
+}
+
+void InputHandler::onMouseButtonDown(SDL_Event &event) {
+  if (event.button.button == SDL_BUTTON_LEFT) {
+    mouseButtonStates[LEFT] = true;
+  }
+  if (event.button.button == SDL_BUTTON_MIDDLE) {
+    mouseButtonStates[MIDDLE] = true;
+  }
+  if (event.button.button == SDL_BUTTON_RIGHT) {
+    mouseButtonStates[RIGHT] = true;
+  }
+}
+
+void InputHandler::onMouseButtonUp(SDL_Event &event) {
+  if (event.button.button == SDL_BUTTON_LEFT) {
+    mouseButtonStates[LEFT] = false;
+  }
+  if (event.button.button == SDL_BUTTON_MIDDLE) {
+    mouseButtonStates[MIDDLE] = false;
+  }
+  if (event.button.button == SDL_BUTTON_RIGHT) {
+    mouseButtonStates[RIGHT] = false;
+  }
+}
+
+// getters
+
 bool InputHandler::isKeyDown(SDL_Scancode key) const {
   return keyStates != 0 && keyStates[key] == 1;
 }
+
+bool InputHandler::getMouseButtonState(int buttonNumber) const {
+  return mouseButtonStates[buttonNumber];
+}
+
+Vector2D *InputHandler::getMousePosition() const { return mousePosition; }
--- inputhandler.h
+++ inputhandler.h
@@ -1,9 +1,14 @@
 #ifndef _INPUT_HANDLER_
 #define _INPUT_HANDLER_
 
+#include "vector.h"
 #include <SDL2/SDL.h>
+#include <vector>
 
 class InputHandler {
+  enum mouseButtons { LEFT = 0, MIDDLE = 1, RIGHT = 2 };
+
+  // singleton pattern
   static InputHandler *instance;
 
   // private ctor to enable singleton pattern.
@@ -14,15 +19,27 @@ class InputHandler {
 
   void onKeyDown();
   void onKeyUp();
+  void onMouseMove(SDL_Event &event);
+  void onMouseButtonDown(SDL_Event &event);
+  void onMouseButtonUp(SDL_Event &event);
+
+  std::vector<bool> mouseButtonStates;
+  Vector2D *mousePosition;
 
 public:
   static InputHandler *Instance();
 
   void update();
   void clean();
+  void reset();
 
   // getters
   bool isKeyDown(SDL_Scancode key) const;
+  bool getMouseButtonState(int buttonNumber) const;
+  Vector2D *getMousePosition() const;
+
+  // dtor
+  ~InputHandler();
 
 }; // class InputHandler
 
